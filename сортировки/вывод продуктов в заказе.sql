1e6299267bceed53259f8477face0197bb75745d
orlovNikitq <130779328+orlovNikitq@users.noreply.github.com>
2023-12-12 17:26:44 +0300

    Update вывод продуктов в заказе.sql

--- сортировки/вывод продуктов в заказе.sql
+++ сортировки/вывод продуктов в заказе.sql
@@ -1,9 +1,7 @@
 use Pizza
--- Assuming user2 has id = 2, you may need to adjust the condition based on your actual data
 DECLARE @UserId INT;
 SET @UserId = (SELECT id FROM _client WHERE login = 'user2');
 
--- Output the result
 SELECT
     po.id_order AS id_order,
     po.id_product AS id_product,
